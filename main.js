1827862d01db5489c7412af4bf69228382607c2d
phillipug <phillipmusiime@gmail.com>
2020-12-13 06:47:13 +0300

    add Block class

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
-class Block{
-  constructor(index, timestamp, data, previousHash){
+import SHA256 from 'crypto-js/sha256'
+import sha256 from 'crypto-js/sha256'
+
+class Block {
+  constructor(index, timestamp, data, previousHash) {
     this.index = index
     this.timestamp = timestamp
     this.data = data
     this.previousHash = previousHash
-    this.hash = ''
+    this.hash = this.calculateHash()
   }
 
-  calculateHash(){
-    
+  calculateHash() {
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString()
   }
 }
